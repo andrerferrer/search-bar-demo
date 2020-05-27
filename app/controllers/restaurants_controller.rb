400722adf366caca1e4eb0c0b2323f7c6ab7e651
Andre Ferrer <andrerferrer@hotmail.com>
2020-05-27 17:20:40 -0300

    Handle the logic in the controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,9 @@
 class RestaurantsController < ApplicationController
   def index
+    if params[:search]
+      @restaurants = City.find_by(name: params[:search][:query]).restaurants
+    else
+      @restaurants = Restaurant.all
+    end
   end
 end
